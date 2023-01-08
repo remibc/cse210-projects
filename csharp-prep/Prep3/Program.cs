1470e4b961c0f9ba9492300fff6fd4e06059ffea
remibc <bar22012@byui.edu>
2023-01-08 13:17:55 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int mnumber = randomGenerator.Next(1, 100);
+
+        Console.Write("What is your guess? ");
+        string number2 = Console.ReadLine();
+        int guess = int.Parse (number2);
+
+        int x = 0;
+
+        while (guess != mnumber)
+        {
+        if (guess > mnumber)
+        {
+        Console.WriteLine($"Higher");
+        }
+        else if (guess < mnumber)
+        {
+            Console.WriteLine($"Lower");            
+        }
+
+        Console.Write("What is your guess? ");
+        string number3 = Console.ReadLine();
+        guess = int.Parse (number3);
+        x=x+1;
+        }
+
+        Console.WriteLine($"You guessed it! You have {x} guessess");
     }
 }
