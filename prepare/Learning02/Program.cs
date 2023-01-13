2abd7b008d7662584e8473b667f0a38a5d8b2de8
remibc <bar22012@byui.edu>
2023-01-12 19:30:36 -0700

    W03

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1  = new Job();
+        job1._company = "Madata";
+        job1._jobTitle = "Service Desk Agent";
+        job1._startYear = 2019;
+        job1._endYear = 2020;
+        
+        Job job2  = new Job();
+        job2._company = "Robert Bosch";
+        job2._jobTitle = "SAP SD";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+
+        Resume myResume  = new Resume();
+        myResume._name= "Remi Barron";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayJobDetails();
     }
 }
