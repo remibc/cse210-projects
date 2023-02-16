7fdabec27ed960af98d19f9f5f27811d0654ea6b
remibc <bar22012@byui.edu>
2023-02-16 10:58:34 -0700

    week 07

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment  test1= new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(test1.GetSummary());
+
+        MathAssignment test2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(test2.GetSummary());
+        Console.WriteLine(test2.GetHomeworkList());
+
+        WritingAssignment test3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(test3.GetSummary());
+        Console.WriteLine(test3.GetWritingInformation());
     }
 }
