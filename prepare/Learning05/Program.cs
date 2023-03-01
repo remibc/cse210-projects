da67ab335b8fc43740faff05289b626c038b2fe9
remibc <bar22012@byui.edu>
2023-03-01 13:20:22 -0700

    week 09

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square test1 = new Square("Red", 3);
+        //Console.WriteLine(test1.GetArea());
+        shapes.Add(test1);
+
+        Rectangle test2 = new Rectangle("Blue", 4, 5);
+        //Console.WriteLine(test2.GetArea());
+        shapes.Add(test2);
+
+        Circle test3 = new Circle("Green", 6);
+        //Console.WriteLine(test3.GetArea());
+        shapes.Add(test3);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"The {s.GetColor()} shape has an area of {s.GetArea()}.");
+        }
+
     }
 }
